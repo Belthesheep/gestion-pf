7126e234715c76e270fe51eef0c5f44b6ae1c354
Alejandro45859 <ulisesurquiza2@gmail.com>
2025-10-01 10:33:41 -0300

    Hice la barra de botones

--- titanfitness/titansite/src/components/Header.jsx
+++ titanfitness/titansite/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import "../index.css"
+import Buttonbar from "./Buttonbar"
 const Header = () => {
   return (
     <div className="header">
-        <h1>Titan Fitness</h1>   
+        <h1>Titan Fitness</h1>
+        <Buttonbar />   
     </div>
   )
 }
