0744297b0f479192b8d8f4ec8ffdc542dce8d6f6
Jlio <verajulio333@gmail.com>
2025-10-14 20:55:12 -0300

    agrego el sistema de login

--- titanfitness/titansite/src/components/Buttonbar.jsx
+++ titanfitness/titansite/src/components/Buttonbar.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+import LoginModal from './LoginModal';
+import RegisterModal from './RegisterModal';
 
 const Buttonbar = () => {
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+
   return (
-        <nav className="taskbar">
-            <div>
-                <button className="task-button"> Menu</button>
-                <button className="task-button"> Nosotros</button>
-                <button className="task-button"> Membresias</button>
-                <button className="task-button"> Contactos</button>
-                <button className="task-button"> Reportes</button>
-            </div>
+    <nav className="taskbar">
+      <div>
+        <button className="task-button">Menu</button>
+        <button className="task-button">Nosotros</button>
+        <button className="task-button">Membresias</button>
+        <button className="task-button">Contactos</button>
+        <button className="task-button">Reportes</button>
+      </div>
 
-            <div className="login-group">
-                <button className="login-button">Login</button>
-                <button className="login-button">Create</button>
-            </div>
-        </nav>
-  )
-}
+      <div className="login-group">
+        {/* Botón para abrir el modal de Login */}
+        <button 
+          className="login-button" 
+          onClick={() => setShowLogin(true)}
+        >
+          Login
+        </button>
 
-export default Buttonbar
+        {/* Botón para abrir el modal de Registro */}
+        <button 
+          className="login-button" 
+          onClick={() => setShowRegister(true)}
+        >
+          Create
+        </button>
+      </div>
+
+      {/* Mostrar los modales cuando sea necesario */}
+      {showLogin && <LoginModal onClose={() => setShowLogin(false)} />}
+      {showRegister && <RegisterModal onClose={() => setShowRegister(false)} />}
+    </nav>
+  );
+};
+
+export default Buttonbar;
 
 
      
