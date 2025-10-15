d5acf1915413e23e17f9063e78e7fbf3d9996179
Alejandro45859 <ulisesurquiza2@gmail.com>
2025-10-14 23:33:27 -0300

    Revert "Agregamos el sistema de reporte"

--- titanfitness/titansite/src/App.jsx
+++ titanfitness/titansite/src/App.jsx
@@ -1,40 +1,13 @@
- feature/trainer-report
-import { useState, useEffect } from 'react';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Body from './components/Body'
-import Report from './components/Report'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Body from './components/Body';
- main
 
 function App() {
-  const [mostrarReporte, setMostrarReporte] = useState(false);
-
-  const toggleReporte = () => {
-    console.log("toggleReporte ejecutado");
-    setMostrarReporte(!mostrarReporte);
-  };
-
-  useEffect(() => {
-    const cerrar = () => setMostrarReporte(false);
-    document.addEventListener("cerrarReporteGlobal", cerrar);
-    return () => document.removeEventListener("cerrarReporteGlobal", cerrar);
-  }, []);
-
   return (
     <>
- feature/trainer-report
-      <header><Header onReportClick={toggleReporte}/></header>
-      <Body />
-      {mostrarReporte && <Report />}
-      <footer><Footer /></footer>
-
       <Header />
       <Body />
       <Footer />
- main
     </>
   );
 }
--- titanfitness/titansite/src/components/Header.jsx
+++ titanfitness/titansite/src/components/Header.jsx
@@ -1,12 +1,10 @@
 import "../index.css"
 import Buttonbar from "./Buttonbar"
-
-
-const Header = ({ onReportClick }) => {
+const Header = () => {
   return (
     <div className="header">
         <h1>Titan Fitness</h1>
-        <Buttonbar onReportClick={onReportClick}  />  
+        <Buttonbar />   
     </div>
   )
 }
